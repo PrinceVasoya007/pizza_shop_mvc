c7a79c753433d54b5de53eba5e1b01c9c6fbc5f8
PrinceVasoya007 <tatvasoft.pca30@outlook.com>
2025-02-12 11:26:21 +0530

    1 phase

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pizza_shop_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
