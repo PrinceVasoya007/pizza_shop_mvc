1013046a434f40a48f370893b115c7192c50d75d
PrinceVasoya <vasoyaprinsu111@gmail.com>
2025-02-13 23:03:09 +0530

    sdsd

--- obj/Debug/net7.0/pizza_shop_MVC.AssemblyInfo.cs
+++ obj/Debug/net7.0/pizza_shop_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pizza_shop_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6efe94f2ac326497fd0bf826eeb2861783dcb2cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ce824d6aa82fd23c1276a2d16bc18d100c7294")]
 [assembly: System.Reflection.AssemblyProductAttribute("pizza_shop_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pizza_shop_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
